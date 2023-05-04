44939268c69aba70852e9d592dc8320442bb942e
Joshua Mendoza <joshuam@mailbox.org>
2023-05-03 19:04:03 -0600

    Provide static files in a different path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,9 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static/', 'examples/']
+html_static_path = ['static/']
+
+html_extra_path = ['examples/']
 
 def setup(app):
     try:
